1a689d2083fc9df5fd967f0c81ded5b90b4b8306
saurabh11x <saurabhkarnwal07@gmail.com>
2024-03-19 08:49:08 +0530

    checking3

--- config/db.js
+++ config/db.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 
 
 function connectDb() {
-  const URL = "mongodb+srv://wShare:FOQGU9qF2NRHmE40@cluster0.ensfxjj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true  });
+  mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true  });
   const connection = mongoose.connection;
 
   // connection.once('open', () => {
